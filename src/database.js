803c0c5df95ea074e145d757e1ef49d34b7f7f23
carlitosjimenez550 <cajimenezm2@gmail.com>
2024-06-19 07:43:11 -0500

    cambios

--- src/database.js
+++ src/database.js
@@ -1,12 +1,27 @@
-const mysql = require ("promise-mysql");
+/*const mysql = require ("promise-mysql");
 const dotenv=require("dotenv");/*informacion para consumir el archivo .env*/
-dotenv.config();/*informacion para consumir el archivo .env*/
-const conection = mysql.createcConection({
-    host: process.env.HOST /*"localhost"*/,
+/*dotenv.config();/*informacion para consumir el archivo .env*/
+/*const conection = mysql.createConection({*/
+    /*host: process.env.HOST /*"localhost"*/
+    /*database: process.env.DATABASE /*"sakila"*/
+    /*user: process.env.USER /*"root"*/
+    /*password: process.env.PASSWORD /*"C0m3rc1al.1"*/
+/*})*/
+/*const getConnection =async()=> await conection;
+module.exports={
+    getConnection
+}/*
+
+/*conexion con credenciales directas sin uso de archivo independiente*/
+const mysql = require ("promise-mysql");
+
+const conection = mysql.createConnection({
+    host: "localhost",
     database: "sakila",
     user: "root",
-    password:"C0m3rc1al.1",
+    password: "C0m3rc1al.1",
 })
+
 const getConnection =async()=> await conection;
 module.exports={
     getConnection
--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 const express= require ("express");
 const app=express();
-
+const database = require("./database");
 app.set("port", 4000);
+
+
+app.get("/actor", async(req, res)=>{
+    const conection = await database.getConnection()
+    const resultado = await conection.query("SELECT * from actor");
+    res.json(resultado);
+    /*res.send("mensaje recibido")*/
+
+})
 app.listen(app.get("port"));
 console.log("Escuchando en el puerto" + app.get("port"));
-
-app.get("/actor", (req,res)=>{
-    res.send("mensaje recibido")
-})
-console.log("Es nuevo")
-console.log("Practicando nodemon")
-console.log("repasando")
+console.log("Es nuevo");
+console.log("Practicando nodemon");
+console.log("repasando");
