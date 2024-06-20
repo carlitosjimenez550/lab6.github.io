89f763cc73b39363869d538eb4ef0b72a377f4a5
carlitosjimenez550 <cajimenezm2@gmail.com>
2024-06-20 07:09:21 -0500

    cambios

--- src/database.js
+++ src/database.js
@@ -13,6 +13,7 @@ module.exports={
 }/*
 
 /*conexion con credenciales directas sin uso de archivo independiente*/
+/*const mysql = require('mysql2');
 const mysql = require ("promise-mysql");
 
 const conection = mysql.createConnection({
@@ -25,4 +26,22 @@ const conection = mysql.createConnection({
 const getConnection =async()=> await conection;
 module.exports={
     getConnection
-}
+}*/
+
+const mysql = require('mysql2');
+const {Conecction} = require ("promise-mysql");
+
+const conexion = mysql.createConnection({
+    host: "localhost",
+    database: "sakila",
+    user: "root",
+    password: "C0m3rc1al.1",
+});
+
+conexion.connect(function(err){
+    if(err){
+        throw err;
+    } else {
+        console.log("conexion exitosa");
+    }
+});
